53dfa64cecff411018f193020a31004c5ce7a5c7
DDDSek <70427733+DDDSek@users.noreply.github.com>
2020-11-10 13:11:43 +0200

    Update ContainerTests.ps1

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -1,16 +1,25 @@
 $count = 0
+
+$arrayApis = @("http://localhost", "http://localhost:5000/", "http://localhost:5001/Identity/Login", "http://localhost:5002/dealers/1", "http://localhost:5003/Statistics")
+
 do {
     $count++
+	
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
+	
+	for($i = 0; $i -lt $arrayApis.length; $i++){ 
+	
+		$testStart = Invoke-WebRequest -Uri $arrayApis[$i] -UseBasicParsing
     
-    $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
-    
-    if ($testStart.statuscode -eq '200') {
-        $started = $true
-    } else {
-        Start-Sleep -Seconds 1
-    }
-    
+		if ($testStart.statuscode -eq '200' -Or $testStart.statuscode -eq '401') {
+		  $started = $true
+		} 
+		else {
+		  Start-Sleep -Seconds 5
+		}
+		
+	}
+	
 } until ($started -or ($count -eq 3))
 
 if (!$started) {
