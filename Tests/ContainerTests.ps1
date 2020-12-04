6c9861c7f8dfc4b973fc1b96d7e26e54423fc4c6
ggfd <sekulsfml@gmail.com>
2020-12-04 10:26:23 +0200

    .

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -1,26 +1,16 @@
 $count = 0
-
-$arrayApis = @("http://localhost", "http://localhost:5000/", "http://localhost:5001/Identity/Login", "http://localhost:5002/dealers/1", "http://localhost:5003/Statistics")
-
 do {
     $count++
-	
-      Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
-	
-	for($i = 0; $i -lt $arrayApis.length; $i++){ 
-  
-		$testStart = Invoke-WebRequest -Uri $arrayApis[$i] -UseBasicParsing
+    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
     
-		if ($testStart.statuscode -eq '200' -Or $testStart.statuscode -eq '401') {
-		    $started = $true
-		} 
-		else {
-		  Start-Sleep -Seconds 5 
-		}
-		
-	}
-	
-		 # $started = $false
+    $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
+    
+    if ($testStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+    
 } until ($started -or ($count -eq 3))
 
 if (!$started) {
